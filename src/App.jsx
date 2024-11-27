cc314dbc72e6b7f415da2881ed047f1f44e6f56c
unknown <wissembencheikh@gmail.com>
2024-11-27 18:33:41 +0100

    Initial commit - Setup project structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>Commencez à créer votre CV interactif ici !</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Générateur de CV</h1>
+    </header>
+  );
+};
+
+export default Header;
